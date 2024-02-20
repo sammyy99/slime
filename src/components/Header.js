eb13598e4b7e52311c1c26099a53bbdc37f58c17
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-02-20 16:55:57 +0530

    Debouncing applied in search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { sidebarToggle } from "../Redux/headerSlice";
 import YouTubeIcon from "@mui/icons-material/YouTube";
@@ -6,13 +6,35 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Face5Icon from "@mui/icons-material/Face5";
 import SearchIcon from "@mui/icons-material/Search";
 import { Link } from "react-router-dom";
+import { youtubeSearchApi } from "../constants/urls";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchSuggestions, setSearchSuggestions] = useState([]);
+  const [showSearchResults, setShowSearchResults] = useState(false)
+
+  const search = useRef(null);
   const dispatch = useDispatch();
 
   const handleSidebarToggle = () => {
     dispatch(sidebarToggle());
   };
+
+  const handleSearch = () => {
+    setSearchQuery(search.current.value);
+  };
+
+  const youtubeSuggestion = async (squery) => {
+    const data = await fetch(youtubeSearchApi + squery);
+    const json = await data.json();
+    setSearchSuggestions(json[1]);
+    console.log("API called for"+searchQuery)
+  };
+
+  useEffect(() => {
+   const timer = setTimeout(()=>youtubeSuggestion(searchQuery),200) 
+   return () => clearTimeout(timer);
+  }, [searchQuery]);
 
   return (
     <div className="grid grid-cols-12 shadow-lg py-1 bg-black text-white">
@@ -34,15 +56,41 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="flex justify-center col-start-5 col-end-9 py-1 px-2 rounded-full">
-        <input
-          className="px-4 w-full rounded-tl-full rounded-bl-full border border-gray-800 bg-black"
-          type="text"
-          placeholder="Search here"
-        ></input>
-        <button className="px-6 rounded-tr-full rounded-br-full border border-gray-800 border-l-0 bg-gray-900 text-white">
-          <SearchIcon />
-        </button>
+      <div className="relative col-start-5 col-end-9 py-1 px-2 rounded-full">
+        <div className="grid grid-cols-8">
+          <input
+            ref={search}
+            onChange={() => {
+              handleSearch();
+            }}
+            className="px-4 py-2 w-full rounded-tl-full rounded-bl-full border border-gray-800 bg-black col-span-7"
+            type="text"
+            placeholder="Search here"
+            onFocus={()=>setShowSearchResults(true)}
+            onBlur={()=>setShowSearchResults(false)}
+          ></input>
+          <button className="px-4 rounded-tr-full rounded-br-full border border-gray-800 border-l-0 bg-gray-800 text-white col-span-1">
+            <SearchIcon />
+          </button>
+        </div>
+
+        {showSearchResults &&
+        <div className="text-white bg-gray-800 rounded-xl absolute mt-1 w-[85%]">
+          <ul className="">
+            {!searchSuggestions
+              ? "null"
+              : searchSuggestions.map((result) => {
+                  return (
+                    <li key={result} className="flex px-4 py-1 my-1 hover:bg-gray-600 hover:cursor-pointer">
+                      <div>
+                        <SearchIcon fontSize="small" />
+                      </div>
+                      <div className="pl-2">{result}</div>
+                    </li>
+                  );
+                })}
+          </ul>
+        </div>}
       </div>
 
       <div className="flex justify-end mt-1 col-start-12 px-4 hover:cursor-pointer">
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,3 +1,5 @@
 const GoogleApiKey = 'AIzaSyCdLtLgrRlGmrz-8KRBbpOvoAuG8rn8r6g'
 
 export const YoutubeListApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=40&regionCode=US&key=${GoogleApiKey}`
+
+export const youtubeSearchApi = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
